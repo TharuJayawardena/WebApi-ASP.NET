7bd39f9e4017725cd5137fcf56397ff78165519d
TharushiJayawardena <tharushijayawardena03@gmail.com>
2022-12-28 13:43:08 +0530

    first commit

---
+++ WebApplication1/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiDemo.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [MinLength(10)]
+
+        public string FirstName { get; set; }
+
+
+    }
+}
